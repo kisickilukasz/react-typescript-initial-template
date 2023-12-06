ee870799edc3d7993b556df3c67ae6aad5bfd1f9
Lukasz Kisicki <lukaszkisicki@MacBook-Pro-Lukasz.local>
2023-12-06 15:23:36 +0100

    feat: add some eslint config and clean unnecessary code

--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,8 @@
 import './app.css';
-
-import logo from './icons/logo.svg';
-
 export function App() {
   return (
-    <div className="app" role="main">
-      <header className="app-header">
-        <img src={logo} className="app-logo" alt="logo" />
-        <p>
-          Edit <code>src/app.tsx</code> and save to reload.
-        </p>
-        <a
-          className="app-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>Welcome new app!</div>
     </div>
   );
 }
